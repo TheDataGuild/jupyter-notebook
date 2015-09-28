6539579a126a320273ff5d5286bdf86efb069cdf
Chris Diehl <chris@thedataguild.com>
2015-09-28 16:18:35 -0700

    Added apt-get install unzip to final_config.sh.

--- final_config.sh
+++ final_config.sh
@@ -1,7 +1,7 @@
 mkdir ~/.aws
 mv config ~/.aws
 mv credentials ~/.aws
-sudo apt-get install -y emacs24 python-pip
+sudo apt-get install -y emacs24 python-pip unzip
 sudo pip install awscli
 instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 aws ec2 attach-volume --volume-id $1 --instance-id $instanceid --device /dev/xvdf

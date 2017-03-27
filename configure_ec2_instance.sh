42643ec031d113674a3c55ff6bfac73805b98aac
Chris Diehl <chris@thedataguild.com>
2017-03-27 13:04:17 -0700

    Updated the Anaconda version and made additional modifications to the build and deployment process

--- configure_ec2_instance.sh
+++ configure_ec2_instance.sh
@@ -5,7 +5,7 @@ docker-machine create --driver amazonec2 \
           --amazonec2-ami $AWS_AMI_ID \
           --amazonec2-instance-type $AWS_INSTANCE_TYPE \
           --amazonec2-region $AWS_DEFAULT_REGION \
-          --amazonec2-root-size 40 \
+          --amazonec2-root-size $AWS_ROOT_SIZE \
           --amazonec2-security-group $AWS_SECURITY_GROUP $1
 eval "$(docker-machine env $1)"
 docker-machine scp final_config.sh $1:final_config.sh
--- ipython_config/ipy_final_config.sh
+++ ipython_config/ipy_final_config.sh
@@ -1,4 +1,4 @@
 ipython profile create
-cp /notebooks/ipython_config/line_profiler_ext.py /root/.ipython/extensions/line_profiler_ext.py
-cp /notebooks/ipython_config/memory_profiler_ext.py /root/.ipython/extensions/memory_profiler_ext.py
-cat /notebooks/ipython_config/ipython_config_additions.txt >> /root/.ipython/profile_default/ipython_config.py 
+cp /data/ipython_config/line_profiler_ext.py /root/.ipython/extensions/line_profiler_ext.py
+cp /data/ipython_config/memory_profiler_ext.py /root/.ipython/extensions/memory_profiler_ext.py
+cat /data/ipython_config/ipython_config_additions.txt >> /root/.ipython/profile_default/ipython_config.py 

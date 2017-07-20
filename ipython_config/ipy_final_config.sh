234794e9cdf953256b2b62aff3e60ef8b52e78bb
Chris Diehl <chris@thedataguild.com>
2017-07-20 16:55:26 +1000

    Removing ipython_config/

--- ipython_config/ipy_final_config.sh
+++
@@ -1,4 +0,0 @@
-ipython profile create
-cp /data/ipython_config/line_profiler_ext.py /root/.ipython/extensions/line_profiler_ext.py
-cp /data/ipython_config/memory_profiler_ext.py /root/.ipython/extensions/memory_profiler_ext.py
-cat /data/ipython_config/ipython_config_additions.txt >> /root/.ipython/profile_default/ipython_config.py 
